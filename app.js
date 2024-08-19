4986150081a3eeebf0d540a3202264bccfb33525
MariaShakeel10 <mariashakeel10a@gmail.com>
2024-08-19 20:25:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,143 @@
+// object 
+makeup_products = {
+    "MAC": {
+        "Lipstick": {"img":"images/mac red lip.jfif", "name": "Lipstick", "price": 15.99 ,'class':"mac1","span":"mac1"},
+        "Foundation": {"img":"images/mac fair foundation.jfif", "name": "Foundation", "price": 30.00,'class':"mac2","span":"mac2" },
+        "Eyeliner": {"img":"images/mac black eyeline.jfif", "name": "Eyeliner", "price": 12.50,'class':"mac3","span":"mac3" },
+        "Mascara": { "img":"images/mac black masca.jfif","name": "Mascara", "price": 18.75,'class':"mac4","span":"mac4" },
+        "Blush": {"img":"images/mac pink blush.jfif", "name": "Blush", "price": 22.00,'class':"mac5","span":"mac5" }
+    },
+    "Estée Lauder": {
+        "Lip Gloss": { "name": "Lip Gloss", "price": 14.99 },
+        "Concealer": { "name": "Concealer", "price": 28.00 },
+        "Eye Shadow": { "name": "Eye Shadow", "price": 16.50 },
+        "Bronzer": { "name": "Bronzer", "price": 19.00 },
+        "Highlighter": { "name": "Highlighter", "price": 24.00 }
+    },
+    "Clinique": {
+        "Nail Polish": { "name": "Nail Polish", "price": 8.99 },
+        "Lip Balm": { "name": "Lip Balm", "price": 7.50 },
+        "Setting Spray": { "name": "Setting Spray", "price": 25.00 },
+        "Face Primer": { "name": "Face Primer", "price": 30.50 },
+        "Eyeshadow Palette": { "name": "Eyeshadow Palette", "price": 45.00 }
+    },
+    "NARS": {
+        "Lip Liner": { "name": "Lip Liner", "price": 13.00 },
+        "Face Powder": { "name": "Face Powder", "price": 32.00 },
+        "Brow Pencil": { "name": "Brow Pencil", "price": 11.75 },
+        "Setting Powder": { "name": "Setting Powder", "price": 29.00 },
+        "Tinted Moisturizer": { "name": "Tinted Moisturizer", "price": 35.00 }
+    },
+    "Charlotte Tilbury": {
+        "Mascara": { "name": "Mascara", "price": 20.00 },
+        "Eyeliner": { "name": "Eyeliner", "price": 14.50 },
+        "Lipstick": { "name": "Lipstick", "price": 16.00 },
+        "Face Serum": { "name": "Face Serum", "price": 40.00 },
+        "Concealer": { "name": "Concealer", "price": 27.00 }
+    },
+    "Benefit": {
+        "Blush": { "name": "Blush", "price": 23.00 },
+        "Foundation": { "name": "Foundation", "price": 32.50 },
+        "Lip Gloss": { "name": "Lip Gloss", "price": 12.00 },
+        "Eye Shadow": { "name": "Eye Shadow", "price": 18.25 },
+        "Highlighter": { "name": "Highlighter", "price": 26.00 }
+    },
+    "Urban Decay": {
+        "Eyeliner": { "name": "Eyeliner", "price": 15.50 },
+        "Lip Balm": { "name": "Lip Balm", "price": 6.75 },
+        "Face Mist": { "name": "Face Mist", "price": 20.00 },
+        "Blush": { "name": "Blush", "price": 21.50 },
+        "Eye Shadow": { "name": "Eye Shadow", "price": 17.00 }
+    },
+    "Bobbi Brown": {
+        "Lipstick": { "name": "Lipstick", "price": 19.99 },
+        "Mascara": { "name": "Mascara", "price": 22.00 },
+        "Foundation": { "name": "Foundation", "price": 31.50 },
+        "Highlighter": { "name": "Highlighter", "price": 27.50 },
+        "Concealer": { "name": "Concealer", "price": 29.00 }
+    },
+    "Lancôme": {
+        "Face Primer": { "name": "Face Primer", "price": 28.50 },
+        "Lip Liner": { "name": "Lip Liner", "price": 12.00 },
+        "Blush": { "name": "Blush", "price": 24.00 },
+        "Nail Polish": { "name": "Nail Polish", "price": 9.50 },
+        "Setting Spray": { "name": "Setting Spray", "price": 26.00 }
+    },
+    "Too Faced": {
+        "Eye Shadow Palette": { "name": "Eye Shadow Palette", "price": 48.00 },
+        "Lip Gloss": { "name": "Lip Gloss", "price": 13.50 },
+        "Face Powder": { "name": "Face Powder", "price": 31.00 },
+        "Concealer": { "name": "Concealer", "price": 28.00 },
+        "Mascara": { "name": "Mascara", "price": 21.50 }
+    },
+    "Tarte": {
+        "Lipstick": { "name": "Lipstick", "price": 17.00 },
+        "Foundation": { "name": "Foundation", "price": 33.00 },
+        "Eyeliner": { "name": "Eyeliner", "price": 14.25 },
+        "Blush": { "name": "Blush", "price": 22.50 },
+        "Highlighter": { "name": "Highlighter", "price": 25.00 }
+    },
+    "Laura Mercier": {
+        "Lip Balm": { "name": "Lip Balm", "price": 7.00 },
+        "Eye Shadow": { "name": "Eye Shadow", "price": 17.50 },
+        "Mascara": { "name": "Mascara", "price": 20.50 },
+        "Face Primer": { "name": "Face Primer", "price": 29.00 },
+        "Highlighter": { "name": "Highlighter", "price": 26.50 }
+    },
+    "Huda Beauty": {
+        "Blush": { "name": "Blush", "price": 24.00 },
+        "Lip Gloss": { "name": "Lip Gloss", "price": 14.00 },
+        "Foundation": { "name": "Foundation", "price": 34.00 },
+        "Eyeliner": { "name": "Eyeliner", "price": 13.75 },
+        "Eye Shadow": { "name": "Eye Shadow", "price": 19.00 }
+    },
+    "Make Up For Ever": {
+        "Concealer": { "name": "Concealer", "price": 30.00 },
+        "Setting Spray": { "name": "Setting Spray", "price": 27.00 },
+        "Lip Liner": { "name": "Lip Liner", "price": 12.50 },
+        "Face Powder": { "name": "Face Powder", "price": 33.00 },
+        "Nail Polish": { "name": "Nail Polish", "price": 10.00 }
+    },
+    "Giorgio Armani": {
+        "Mascara": { "name": "Mascara", "price": 21.00 },
+        "Blush": { "name": "Blush", "price": 22.00 },
+        "Eye Shadow Palette": { "name": "Eye Shadow Palette", "price": 44.00 },
+        "Highlighter": { "name": "Highlighter", "price": 28.00 },
+        "Lipstick": { "name": "Lipstick", "price": 16.50 }
+    },
+    "Fenty Beauty": {
+        "Foundation": { "name": "Foundation", "price": 35.00 },
+        "Lip Gloss": { "name": "Lip Gloss", "price": 13.75 },
+        "Eyeliner": { "name": "Eyeliner", "price": 14.00 },
+        "Concealer": { "name": "Concealer", "price": 29.50 },
+        "Setting Spray": { "name": "Setting Spray", "price": 28.00 }
+    }
+}
+// object end
+
+// targeting elements
+const main = document.getElementById("main")
+const color = document.getElementById("color")
+const brand = document.getElementById("brand")
+const item = document.getElementById("item")
+
+// end of targetting
+
+// making cards
+for (const key in makeup_products) {
+    let allProductsBrand = makeup_products[key];
+    brand.innerHTML +=`<li><a class="dropdown-item" href="#" value="${key}">${key}</a></li>`
+    for (const key1 in makeup_products[key]) {
+        let allProducts = makeup_products[key][key1];
+
+        main.innerHTML += `<div class="card text-center" style="width: 18rem;">
+  <img src="${allProducts.img}" id="${allProducts.class}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${allProducts.name}</h5>
+    <p class="card-text">Price: ${allProducts.price}$</p>
+    <div id="color ${allProducts.span}"></div>
+    <a href="#" class="btn btn-primary">Add To Cart</a>
+  </div>
+</div>`
+    }
+}
