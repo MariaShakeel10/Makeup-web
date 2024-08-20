9a9230696d772158a3ebdcd54e3566a1a363afca
MariaShakeel10 <mariashakeel10a@gmail.com>
2024-08-20 20:30:29 +0500

    second commit

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
 // object 
 makeup_products = {
     "MAC": {
-        "Lipstick": {"img":"images/mac red lip.jfif", "name": "Lipstick", "price": 15.99 ,'class':"mac1","span":"mac1"},
-        "Foundation": {"img":"images/mac fair foundation.jfif", "name": "Foundation", "price": 30.00,'class':"mac2","span":"mac2" },
-        "Eyeliner": {"img":"images/mac black eyeline.jfif", "name": "Eyeliner", "price": 12.50,'class':"mac3","span":"mac3" },
-        "Mascara": { "img":"images/mac black masca.jfif","name": "Mascara", "price": 18.75,'class':"mac4","span":"mac4" },
-        "Blush": {"img":"images/mac pink blush.jfif", "name": "Blush", "price": 22.00,'class':"mac5","span":"mac5" }
+        "Lipstick": { "img": "images/mac red lip.jfif", "name": "Lipstick", "price": 15.99, },
+        "Foundation": { "img": "images/mac fair foundation.jfif", "name": "Foundation", "price": 30.00, },
+        "Eyeliner": { "img": "images/mac black eyeline.jfif", "name": "Eyeliner", "price": 12.50, },
+        "Mascara": { "img": "images/mac black masca.jfif", "name": "Mascara", "price": 18.75, },
+        "Blush": { "img": "images/mac pink blush.jfif", "name": "Blush", "price": 22.00, }
     },
     "Estée Lauder": {
-        "Lip Gloss": { "name": "Lip Gloss", "price": 14.99 },
-        "Concealer": { "name": "Concealer", "price": 28.00 },
-        "Eye Shadow": { "name": "Eye Shadow", "price": 16.50 },
-        "Bronzer": { "name": "Bronzer", "price": 19.00 },
-        "Highlighter": { "name": "Highlighter", "price": 24.00 }
+        "Lip Gloss": {"img":"images/est lip pink.jfif", "name": "Lip Gloss", "price": 14.99 },
+        "Concealer": {"img":"images/est conc fair.jfif", "name": "Concealer", "price": 28.00 },
+        "Eye Shadow": {"img":"images/est eyes.jfif", "name": "Eye Shadow", "price": 16.50 },
+        "Bronzer": {"img":"images/est bronz light.jfif", "name": "Bronzer", "price": 19.00 },
+        "Highlighter": {"img":"images/est high silver.jfif", "name": "Highlighter", "price": 24.00 }
     },
     "Clinique": {
-        "Nail Polish": { "name": "Nail Polish", "price": 8.99 },
-        "Lip Balm": { "name": "Lip Balm", "price": 7.50 },
-        "Setting Spray": { "name": "Setting Spray", "price": 25.00 },
-        "Face Primer": { "name": "Face Primer", "price": 30.50 },
-        "Eyeshadow Palette": { "name": "Eyeshadow Palette", "price": 45.00 }
+        "Nail Polish": {"img":"images/clini nail.jfif", "name": "Nail Polish", "price": 8.99 },
+        "Lip Balm": {"img":"images/clini lipblam.jfif", "name": "Lip Balm", "price": 7.50 },
+        "Setting Spray": {"img":"images/clini settt.jfif", "name": "Setting Spray", "price": 25.00 },
+        "Face Primer": {"img":"images/clini face primer.jfif", "name": "Face Primer", "price": 30.50 },
+        "Eyeshadow Palette": {"img":"images/clini eye shado.jfif", "name": "Eyeshadow Palette", "price": 45.00 }
     },
     "NARS": {
-        "Lip Liner": { "name": "Lip Liner", "price": 13.00 },
-        "Face Powder": { "name": "Face Powder", "price": 32.00 },
-        "Brow Pencil": { "name": "Brow Pencil", "price": 11.75 },
-        "Setting Powder": { "name": "Setting Powder", "price": 29.00 },
-        "Tinted Moisturizer": { "name": "Tinted Moisturizer", "price": 35.00 }
+        "Lip Liner": {"img":"images/nars red lip.jfif", "name": "Lip Liner", "price": 13.00 },
+        "Face Powder": {"img":"images/nars face powder fair.jfif", "name": "Face Powder", "price": 32.00 },
+        "Brow Pencil": {"img":"images/nars brow pencil brown.jfif", "name": "Brow Pencil", "price": 11.75 },
+        "Setting Powder": {"img":"images/nars settin sp.jfif", "name": "Setting Powder", "price": 29.00 },
+        "Tinted Moisturizer": {"img":"images/nars tinted moistur.jfif", "name": "Tinted Moisturizer", "price": 35.00 }
     },
     "Charlotte Tilbury": {
-        "Mascara": { "name": "Mascara", "price": 20.00 },
-        "Eyeliner": { "name": "Eyeliner", "price": 14.50 },
-        "Lipstick": { "name": "Lipstick", "price": 16.00 },
-        "Face Serum": { "name": "Face Serum", "price": 40.00 },
-        "Concealer": { "name": "Concealer", "price": 27.00 }
+        "Mascara": {"img":"images/char mascar.jfif", "name": "Mascara", "price": 20.00 },
+        "Eyeliner": {"img":"images/charlo eyeliner.jfif", "name": "Eyeliner", "price": 14.50 },
+        "Lipstick": {"img":"images/charlo red lipstick.jfif", "name": "Lipstick", "price": 16.00 },
+        "Face Serum": {"img":"images/char face serum.jfif", "name": "Face Serum", "price": 40.00 },
+        "Concealer": {"img":"images/charlotte conceal.jfif", "name": "Concealer", "price": 27.00 }
     },
     "Benefit": {
-        "Blush": { "name": "Blush", "price": 23.00 },
-        "Foundation": { "name": "Foundation", "price": 32.50 },
-        "Lip Gloss": { "name": "Lip Gloss", "price": 12.00 },
-        "Eye Shadow": { "name": "Eye Shadow", "price": 18.25 },
-        "Highlighter": { "name": "Highlighter", "price": 26.00 }
+        "Blush": {"img":"images/bene blush pink.jfif", "name": "Blush", "price": 23.00 },
+        "Foundation": {"img":"images/bene foundat.jfif", "name": "Foundation", "price": 32.50 },
+        "Lip Gloss": {"img":"images/bene lipgloss.jfif", "name": "Lip Gloss", "price": 12.00 },
+        "Eye Shadow": {"img":"images/bene eyeshadow.jfif", "name": "Eye Shadow", "price": 18.25 },
+        "Highlighter": {"img":"images/bene rose high.jfif", "name": "Highlighter", "price": 26.00 }
     },
     "Urban Decay": {
-        "Eyeliner": { "name": "Eyeliner", "price": 15.50 },
-        "Lip Balm": { "name": "Lip Balm", "price": 6.75 },
-        "Face Mist": { "name": "Face Mist", "price": 20.00 },
-        "Blush": { "name": "Blush", "price": 21.50 },
-        "Eye Shadow": { "name": "Eye Shadow", "price": 17.00 }
+        "Eyeliner": {"img":"images/urban eyeliner.jfif", "name": "Eyeliner", "price": 15.50 },
+        "Lip Balm": {"img":"images/urban lip blam.jfif", "name": "Lip Balm", "price": 6.75 },
+        "Face Mist": {"img":"images/urban face mist.jfif", "name": "Face Mist", "price": 20.00 },
+        "Blush": {"img":"images/urbam blush.jfif", "name": "Blush", "price": 21.50 },
+        "Eye Shadow": {"img":"images/urban eyeshado.jfif", "name": "Eye Shadow", "price": 17.00 }
     },
     "Bobbi Brown": {
         "Lipstick": { "name": "Lipstick", "price": 19.99 },
@@ -120,15 +120,28 @@ const main = document.getElementById("main")
 const color = document.getElementById("color")
 const brand = document.getElementById("brand")
 const item = document.getElementById("item")
+const selected =document.getElementById("second")
 
 // end of targetting
+
+
+// for loop
+let idImg = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty", "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five", "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine", "forty", "forty-one", "forty-two", "forty-three", "forty-four", "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine", "fifty", "fifty-one", "fifty-two", "fifty-three", "fifty-four", "fifty-five", "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine", "sixty", "sixty-one", "sixty-two", "sixty-three", "sixty-four", "sixty-five", "sixty-six", "sixty-seven", "sixty-eight", "sixty-nine", "seventy", "seventy-one", "seventy-two", "seventy-three", "seventy-four", "seventy-five"]
+let idColor = ["c-one", "c-two", "c-three", "c-four", "c-five", "c-six", "c-seven", "c-eight", "c-nine", "c-ten", "c-eleven", "c-twelve", "c-thirteen", "c-fourteen", "c-fifteen", "c-sixteen", "c-seventeen", "c-eighteen", "c-nineteen", "c-twenty", "c-twenty-one", "c-twenty-two", "c-twenty-three", "c-twenty-four", "c-twenty-five", "c-twenty-six", "c-twenty-seven", "c-twenty-eight", "c-twenty-nine", "c-thirty", "c-thirty-one", "c-thirty-two", "c-thirty-three", "c-thirty-four", "c-thirty-five", "c-thirty-six", "c-thirty-seven", "c-thirty-eight", "c-thirty-nine", "c-forty", "c-forty-one", "c-forty-two", "c-forty-three", "c-forty-four", "c-forty-five", "c-forty-six", "c-forty-seven", "c-forty-eight", "c-forty-nine", "c-fifty", "c-fifty-one", "c-fifty-two", "c-fifty-three", "c-fifty-four", "c-fifty-five", "c-fifty-six", "c-fifty-seven", "c-fifty-eight", "c-fifty-nine", "c-sixty", "c-sixty-one", "c-sixty-two", "c-sixty-three", "c-sixty-four", "c-sixty-five", "c-sixty-six", "c-sixty-seven", "c-sixty-eight", "c-sixty-nine", "c-seventy", "c-seventy-one", "c-seventy-two", "c-seventy-three", "c-seventy-four", "c-seventy-five"]
+let images = []
 
 // making cards
 for (const key in makeup_products) {
     let allProductsBrand = makeup_products[key];
-    brand.innerHTML +=`<li><a class="dropdown-item" href="#" value="${key}">${key}</a></li>`
+    brand.innerHTML += `<li><a class="dropdown-item" href="#" value="${key}">${key}</a></li>`
     for (const key1 in makeup_products[key]) {
         let allProducts = makeup_products[key][key1];
+        for (let i = 0; i < idImg.length; i++) {
+            allProducts.span = idImg[i]
+        }
+        for (let i = 0; i < idColor.length; i++) {
+            allProducts.class = idColor[i]
+        }
 
         main.innerHTML += `<div class="card text-center" style="width: 18rem;">
   <img src="${allProducts.img}" id="${allProducts.class}" class="card-img-top" alt="...">
@@ -140,4 +153,19 @@ for (const key in makeup_products) {
   </div>
 </div>`
     }
+}
+
+// serach event 
+function search(event) {
+    let userValue =makeup_products[brand.value]
+    selected.innerHTML +=`<div class="card text-center" style="width: 18rem;">
+  <img src="${userValue.img}" id="${userValue.class}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${userValue.name}</h5>
+    <p class="card-text">Price: ${userValueprice}$</p>
+    <div id="color ${userValue.span}"></div>
+    <a href="#" class="btn btn-primary">Add To Cart</a>
+  </div>
+</div>`
+main.style.display="none"
 }
